409d480f618d328b9ee1e45eff8a803da8e409d6
Eloi Pardo <eloipr94@gmail.com>
2020-07-13 14:21:47 +0200

    generate random lorem ipsum text

--- json_generator.js
+++ json_generator.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const loremIpsum = require("lorem-ipsum").loremIpsum;
 
 const imageUrl = "https://picsum.photos/id/{ID_FOTO_INCREMENTAL}/500/500.jpg";
 
@@ -8,8 +9,14 @@ for (let i = 1; i <= 4000; ++i) {
     jsonArray.push({
         id: i,
         photo: imageUrl.replace("{ID_FOTO_INCREMENTAL}", i),
-        text:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quam ante, commodo vel mi at, tincidunt auctor lorem. Aliquam semper vitae nibh vitae cursus. Fusce non pulvinar est. Sed in auctor metus. Morbi sit amet enim nulla. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nam consequat purus augue, a aliquam augue vehicula non. Nunc in efficitur nulla, in volutpat ipsum. Maecenas ultrices gravida pharetra. Vestibulum nec malesuada purus.",
+        text: loremIpsum({
+            count: 2,
+            paragraphLowerBound: 3,
+            paragraphUpperBound: 7,
+            units: "paragraphs",
+        }),
+        // text:
+        //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quam ante, commodo vel mi at, tincidunt auctor lorem. Aliquam semper vitae nibh vitae cursus. Fusce non pulvinar est. Sed in auctor metus. Morbi sit amet enim nulla. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nam consequat purus augue, a aliquam augue vehicula non. Nunc in efficitur nulla, in volutpat ipsum. Maecenas ultrices gravida pharetra. Vestibulum nec malesuada purus.",
     });
 }
 
