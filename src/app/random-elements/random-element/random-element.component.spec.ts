f51a61e050fa5b0d31ccae092dc3a9c33b0ee5e1
Eloi Pardo <eloipr94@gmail.com>
2020-07-14 11:59:41 +0200

    random-element component test

--- src/app/random-elements/random-element/random-element.component.spec.ts
+++ src/app/random-elements/random-element/random-element.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { RandomElementComponent } from "./random-element.component";
+import { HighlightPipe } from "../pipes/highlight.pipe";
 
 describe("RandomElementComponent", () => {
     let component: RandomElementComponent;
@@ -8,17 +9,28 @@ describe("RandomElementComponent", () => {
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-            declarations: [RandomElementComponent],
+            declarations: [RandomElementComponent, HighlightPipe],
         }).compileComponents();
     }));
 
     beforeEach(() => {
         fixture = TestBed.createComponent(RandomElementComponent);
         component = fixture.componentInstance;
+        component.randomElement = {
+            id: 1,
+            photo: "invalid_link",
+            text: "Lorem ipsum.",
+        };
         fixture.detectChanges();
     });
 
     it("should create", () => {
         expect(component).toBeTruthy();
+    });
+
+    it("#onImgError should change the image src", () => {
+        const img = fixture.debugElement.nativeElement.querySelector("img");
+        component.onImgError({ target: img });
+        expect(img.getAttribute("src")).toContain("assets/images/no_image.jpg");
     });
 });
