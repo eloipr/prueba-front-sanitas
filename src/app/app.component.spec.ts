fe5362d599c46fb08d1e2b28997f26b0774a54a1
Eloi Pardo <eloipr94@gmail.com>
2020-07-14 12:55:13 +0200

    app component tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,13 @@
 import { TestBed, async } from "@angular/core/testing";
+
 import { AppComponent } from "./app.component";
+import { RandomElementsModule } from "./random-elements/random-elements.module";
 
 describe("AppComponent", () => {
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             declarations: [AppComponent],
+            imports: [RandomElementsModule],
         }).compileComponents();
     }));
 
@@ -18,12 +21,5 @@ describe("AppComponent", () => {
         const fixture = TestBed.createComponent(AppComponent);
         const app = fixture.debugElement.componentInstance;
         expect(app.title).toEqual("prueba-front-sanitas");
-    });
-
-    it("should render title", () => {
-        const fixture = TestBed.createComponent(AppComponent);
-        fixture.detectChanges();
-        const compiled = fixture.debugElement.nativeElement;
-        expect(compiled.querySelector(".content span").textContent).toContain("prueba-front-sanitas app is running!");
     });
 });
